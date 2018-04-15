0ef61d7a419cdc42520b3d3f8b97948a030dcd5d
shunnnosukekawai <gjpamtp@gmail.com>
2018-04-15 18:21:44 +0900

    アクセスキーなどを別のファイルに移動

--- tweetbot.py
+++ tweetbot.py
@@ -3,10 +3,11 @@ from requests_oauthlib import OAuth1Session
 import json
 import random
 import datetime
-CK = 'BvIg7lWfYhl9RHe7p3erlxLNf'  #consumerkey
-CS = 'Yo1tsibL1KNDoiRpipPyN8UohI4Wq3dTwKlAwbdJAEN4Z0l9ZT' #consumer secret
-AT = '825155594365526016-gfooV9FnXL5z98Pue4CkpC6nDfOpwjp' # access token
-AS = 'iW4hmqiVfAQcsYyQiyoLqaTMhmu1oGzMiMOD6kPnVCbxO' # access token secret
+import setting
+CK = setting.CK 
+CS =setting.CS
+AT = setting.AT
+AS = setting.AS
 
 # ツイート投稿用のURL
 url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
